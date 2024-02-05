b365b4b0189d63628b0c79af47680ae441354ad4
Rubin Won <55333006+ruby199@users.noreply.github.com>
2024-02-05 11:31:47 +0900

    Facade Design Pattern

---
+++ Facade/facade.py
@@ -0,0 +1,67 @@
+"""
+[DEF] Facade Design pattern:
+Provides a simplified interface to a complex system of classesm a library or a framework. 
+Used to hide the complexities of the system and provide a cleaner, simplified, and more understandable interface to the clients.
+
+
+Motivation:
+- Balancing complexity and presentation/usability
+- Typical home
+    - Many subsystems (e.g., electrical sanitation)
+    - Complex internal structure (e.g., floor layers)
+    - End user is not exposed to internals
+- Same with software 
+"""
+class Buffer:
+  def __init__(self, width=30, height=20):
+    self.width = width
+    self.height = height
+    self.buffer = [' '] * (width*height)
+
+  def __getitem__(self, item):
+    return self.buffer.__getitem__(item)
+
+  def write(self, text):
+    self.buffer += text
+
+
+class Viewport:
+  def __init__(self, buffer=Buffer()):
+    self.buffer = buffer
+    self.offset = 0
+
+def get_char_at(self, index):
+  return self.buffer[self.offset+index]
+
+class Viewport:
+  def __init__(self, buffer=Buffer()):
+    self.buffer = buffer
+    self.offset = 0
+
+  def get_char_at(self, index):
+    return self.buffer[self.offset+index]
+
+  def append(self, text):
+    self.buffer += text
+
+
+class Console:
+  def __init__(self):
+    b = Buffer()
+    self.current_viewport = Viewport(b)
+    self.buffers = [b]
+    self.viewports = [self.current_viewport]
+
+  # high-level
+  def write(self, text):
+    self.current_viewport.buffer.write(text)
+
+  # low-level
+  def get_char_at(self, index):
+    return self.current_viewport.get_char_at(index)
+
+
+if __name__ == '__main__':
+  c = Console()
+  c.write('hello')
+  ch = c.get_char_at(0)
